5c1cc95d961fc21f8b0d31dc1c9fa328f2e108ba
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2023-10-03 09:59:43 +0630

    kkk

---
+++ lib/src/services/products_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:e_commerce/src/constants/api_constants.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class ProductsService {
+  final storage = FlutterSecureStorage();
+
+  Future<Map<String, dynamic>> getProductsData(
+      Map<String, dynamic> body) async {
+    var token = await storage.read(key: "token");
+    final response = await http.post(
+      Uri.parse(ApiConstants.productsUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(body),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to log in: ${response.statusCode}');
+    }
+  }
+}
