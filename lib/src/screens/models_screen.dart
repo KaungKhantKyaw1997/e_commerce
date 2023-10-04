aa462f54922c5b11a9f2cea4e772a068a1d7f177
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2023-10-04 14:29:34 +0630

    kkk

---
+++ lib/src/screens/models_screen.dart
@@ -0,0 +1,233 @@
+import 'package:e_commerce/global.dart';
+import 'package:e_commerce/routes.dart';
+import 'package:e_commerce/src/constants/color_constants.dart';
+import 'package:e_commerce/src/constants/font_constants.dart';
+import 'package:e_commerce/src/services/models_service.dart';
+import 'package:e_commerce/src/utils/toast.dart';
+import 'package:flutter/material.dart';
+import 'package:number_paginator/number_paginator.dart';
+
+class ModelsScreen extends StatefulWidget {
+  const ModelsScreen({super.key});
+
+  @override
+  State<ModelsScreen> createState() => _ModelsScreenState();
+}
+
+class _ModelsScreenState extends State<ModelsScreen> {
+  final modelsService = ModelsService();
+  TextEditingController search = TextEditingController(text: '');
+  final ScrollController _modelsController = ScrollController();
+  List models = [];
+  int page = 1;
+  int pageCounts = 0;
+  int total = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    getModels();
+  }
+
+  @override
+  void dispose() {
+    modelsService.cancelRequest();
+    super.dispose();
+  }
+
+  getModels() async {
+    try {
+      final response =
+          await modelsService.getModelsData(page: page, search: search.text);
+      if (response!["code"] == 200) {
+        if (response["data"].isNotEmpty) {
+          models = response["data"];
+          page = response["page"];
+          pageCounts = response["page_counts"];
+          total = response["total"];
+        }
+        setState(() {});
+      } else {
+        ToastUtil.showToast(response["code"], response["message"]);
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  modelCard(index) {
+    return Container(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 8,
+        bottom: 8,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(index == 0 ? 10 : 0),
+          topRight: Radius.circular(index == 0 ? 10 : 0),
+          bottomLeft: Radius.circular(index == models.length - 1 ? 10 : 0),
+          bottomRight: Radius.circular(index == models.length - 1 ? 10 : 0),
+        ),
+        color: Colors.white,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 80,
+            height: 80,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                // image: NetworkImage(
+                //     '${ApiConstants.baseUrl}${models[index]["logo_url"].toString()}'),
+                image: AssetImage("assets/images/gshock1.png"),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.only(
+                left: 4,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    models[index]["name"].toString(),
+                    style: FontConstants.body1,
+                  ),
+                  Text(
+                    models[index]["description"].toString(),
+                    overflow: TextOverflow.ellipsis,
+                    style: FontConstants.caption1,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
+        title: TextField(
+          controller: search,
+          keyboardType: TextInputType.text,
+          textInputAction: TextInputAction.done,
+          autofocus: true,
+          style: FontConstants.body1,
+          cursorColor: Colors.black,
+          decoration: InputDecoration(
+            hintText: language["Search Model"] ?? "Search Model",
+            filled: true,
+            fillColor: ColorConstants.fillcolor,
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide.none,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide.none,
+            ),
+          ),
+          onChanged: (value) {
+            getModels();
+          },
+        ),
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.only(
+            left: 16,
+            right: 16,
+            bottom: 24,
+          ),
+          width: double.infinity,
+          child: Column(
+            children: [
+              models.isNotEmpty
+                  ? Container(
+                      padding: EdgeInsets.only(
+                        top: 4,
+                        bottom: 4,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Total ${total.toString()}',
+                            style: FontConstants.caption1,
+                          ),
+                          NumberPaginator(
+                            numberPages: pageCounts,
+                            onPageChange: (int index) {
+                              setState(() {
+                                page = index + 1;
+                                getModels();
+                              });
+                            },
+                            config: const NumberPaginatorUIConfig(
+                              mode: ContentDisplayMode.hidden,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : Container(),
+              ListView.builder(
+                controller: _modelsController,
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: models.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {},
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        modelCard(index),
+                        index < models.length - 1
+                            ? Container(
+                                padding: const EdgeInsets.only(
+                                  left: 100,
+                                  right: 16,
+                                ),
+                                child: const Divider(
+                                  height: 0,
+                                  color: Colors.grey,
+                                ),
+                              )
+                            : Container(),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
