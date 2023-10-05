3d0001592027a6e4b4fd075fb181e966afeeec5a
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2023-10-05 17:08:39 +0630

    kkk

--- lib/palette.dart
+++ lib/palette.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 
 class Palette {
   static const MaterialColor kToDark = MaterialColor(
-    0xff000000,
+    0xff263238,
     <int, Color>{
-      50: Color(0xffffffff), //10%
-      100: Color(0xffe6e6e6), //20%
-      200: Color(0xffcccccc), //30%
-      300: Color(0xffb3b3b3), //40%
-      400: Color(0xff999999), //50%
-      500: Color(0xff808080), //60%
-      600: Color(0xff666666), //70%
-      700: Color(0xff4d4d4d), //80%
-      800: Color(0xff333333), //90%
-      900: Color(0xff1a1a1a), //100%
+      50: Color(0xffd3d6d7), // 10%
+      100: Color(0xffd3d6d7), // 20%
+      200: Color(0xffbdc1c3), // 30%
+      300: Color(0xffa8adaf), // 40%
+      400: Color(0xff92989b), // 50%
+      500: Color(0xff7c8487), // 60%
+      600: Color(0xff676f73), // 70%
+      700: Color(0xff515a5f), // 80%
+      800: Color(0xff3b464b), // 90%
+      900: Color(0xff263238), // 100%
     },
   );
 }
