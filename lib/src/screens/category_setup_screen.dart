217ca402b0c2dccb78e8a5327765e8f390945feb
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2023-10-12 15:00:15 +0630

    kkk

---
+++ lib/src/screens/category_setup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategorySetupScreen extends StatefulWidget {
+  const CategorySetupScreen({super.key});
+
+  @override
+  State<CategorySetupScreen> createState() => _CategorySetupScreenState();
+}
+
+class _CategorySetupScreenState extends State<CategorySetupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/src/screens/product_setup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductSetupScreen extends StatefulWidget {
+  const ProductSetupScreen({super.key});
+
+  @override
+  State<ProductSetupScreen> createState() => _ProductSetupScreenState();
+}
+
+class _ProductSetupScreenState extends State<ProductSetupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
