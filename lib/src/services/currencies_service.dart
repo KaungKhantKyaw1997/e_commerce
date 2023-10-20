f00760e228f60052c700a1271d58a0beacae0cd2
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2023-10-20 12:05:01 +0630

    currencies

---
+++ lib/src/services/currencies_service.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:e_commerce/src/constants/api_constants.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class CurrenciesService {
+  final storage = FlutterSecureStorage();
+
+  final Dio dio = Dio();
+  CancelToken _cancelToken = CancelToken();
+
+  Future<Map<String, dynamic>?> getCurrenciesData() async {
+    var token = await storage.read(key: "token") ?? '';
+    try {
+      final response = await dio.get(
+        ApiConstants.currenciesUrl,
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            if (token.isNotEmpty) 'Authorization': 'Bearer $token',
+          },
+        ),
+        cancelToken: _cancelToken,
+      );
+
+      return response.data;
+    } catch (e) {
+      print('Error: $e');
+      return null;
+    }
+  }
+
+  void cancelRequest() {
+    _cancelToken.cancel('Request canceled');
+  }
+}
