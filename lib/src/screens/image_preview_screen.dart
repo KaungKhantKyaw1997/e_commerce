80e92f8bdfee820d4234b8d6f0968a1b8f6ed46c
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-10-23 16:01:25 +0630

    update

--- lib/src/screens/image_preview_screen.dart
+++ lib/src/screens/image_preview_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:e_commerce/src/constants/api_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 
@@ -8,14 +11,59 @@ class ImagePreviewScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final arguments =
         ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
-    var image_url = '';
+    var imageUrl = '';
+    var defaultImageUrl = '';
     if (arguments != null) {
-      image_url = arguments["image_url"] as String;
+      defaultImageUrl = arguments["image_url"] as String;
+      var imgParts =
+          defaultImageUrl.replaceAll(ApiConstants.baseUrl, "").split(".");
+      imageUrl =
+          "${ApiConstants.baseUrl}${imgParts[0]}_original.${imgParts[1]}";
     }
 
-    return Container(
-        child: PhotoView(
-      imageProvider: NetworkImage(image_url),
-    ));
+    return FutureBuilder(
+      future: _checkImage(imageUrl),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.error != null) {
+          // If we run into an error, return the default image
+          return Container(
+            child: PhotoView(
+              imageProvider: NetworkImage(defaultImageUrl),
+            ),
+          );
+        } else {
+          // If the image loads successfully, return it
+          return Container(
+            child: PhotoView(
+              imageProvider: NetworkImage(imageUrl),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Future<void> _checkImage(String url) {
+    final Completer<void> completer = Completer();
+
+    final ImageStream stream = NetworkImage(url).resolve(ImageConfiguration());
+    stream.addListener(
+      ImageStreamListener(
+        (info, call) {
+          if (!completer.isCompleted) {
+            completer.complete();
+          }
+        },
+        onError: (exception, stackTrace) {
+          if (!completer.isCompleted) {
+            completer.completeError('Image not found');
+          }
+        },
+      ),
+    );
+
+    return completer.future;
   }
 }
