edf97be9e1d378d99a8dcbf8cc7b8186843eb9e6
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2023-11-24 12:07:23 +0630

    kkk

--- lib/src/constants/font_constants.dart
+++ lib/src/constants/font_constants.dart
@@ -3,138 +3,138 @@ import 'package:flutter/material.dart';
 
 class FontConstants {
   static TextStyle headline1 = TextStyle(
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: FontWeight.w500,
     color: Colors.black,
   );
 
   static TextStyle subheadline1 = TextStyle(
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: FontWeight.w500,
     color: Colors.black,
   );
 
   static TextStyle subheadline2 = TextStyle(
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: FontWeight.w500,
     color: Colors.grey,
   );
 
   static TextStyle subheadline3 = TextStyle(
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: FontWeight.w500,
     color: Colors.white,
   );
 
   static TextStyle body1 = TextStyle(
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: FontWeight.w500,
     color: Colors.black,
   );
 
   static TextStyle body2 = TextStyle(
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: FontWeight.w500,
     color: Colors.grey,
   );
 
   static TextStyle body3 = TextStyle(
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: FontWeight.w500,
     color: Colors.white,
   );
 
   static TextStyle caption1 = TextStyle(
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     color: Colors.grey,
   );
 
   static TextStyle caption2 = TextStyle(
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     color: Colors.black,
   );
 
   static TextStyle caption3 = TextStyle(
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: FontWeight.w500,
     color: Colors.black,
   );
 
   static TextStyle caption4 = TextStyle(
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     color: Colors.white,
   );
 
   static TextStyle caption5 = TextStyle(
     color: Colors.grey,
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     decoration: TextDecoration.underline,
     decorationColor: Colors.grey,
   );
 
   static TextStyle smallText1 = TextStyle(
-    fontSize: 12,
+    fontSize: 10,
     fontWeight: FontWeight.w400,
     color: Colors.grey,
   );
 
   static TextStyle smallText2 = TextStyle(
-    fontSize: 12,
+    fontSize: 10,
     fontWeight: FontWeight.w400,
     color: Colors.black,
   );
 
   static TextStyle smallText3 = TextStyle(
-    fontSize: 12,
+    fontSize: 10,
     fontWeight: FontWeight.w400,
     color: Colors.white,
   );
 
   static TextStyle button1 = TextStyle(
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     color: Colors.white,
   );
 
   static TextStyle button2 = TextStyle(
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     color: Colors.black,
   );
 
   static TextStyle button3 = TextStyle(
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     color: ColorConstants.redColor,
   );
 
   static TextStyle button4 = TextStyle(
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     color: ColorConstants.primaryColor,
   );
 
   static TextStyle title1 = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w400,
-    color: Colors.black,
-  );
-
-  static TextStyle title2 = TextStyle(
-    fontSize: 26,
-    fontWeight: FontWeight.w400,
-    color: Colors.black,
-  );
-
-  static TextStyle subtitle1 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w400,
     color: Colors.black,
   );
 
-  static double bottom = 12;
+  static TextStyle title2 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  static TextStyle subtitle1 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  static double bottom = 10;
 }
